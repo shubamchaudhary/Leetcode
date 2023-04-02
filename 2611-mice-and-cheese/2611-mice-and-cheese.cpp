444ef74c1a30ea29a87184c7f75bb3b400cc8e0f
SHUBAM CHAUDHARY <110082517+shubamchaudhary@users.noreply.github.com>
2023-04-03 00:16:52 +0530

    Time: 284 ms (36.36%), Space: 106.1 MB (9.09%) - LeetHub

--- 2611-mice-and-cheese/2611-mice-and-cheese.cpp
+++ 2611-mice-and-cheese/2611-mice-and-cheese.cpp
@@ -20,50 +20,45 @@ public:
 void heapify(vector<int> &diff,vector<int> &r1,vector<int> &r2, int N, int i)
 {
  
-    // Initialize largest as root
+    
     int largest = i;
  
-    // left = 2*i + 1
     int l = 2 * i + 1;
  
-    // right = 2*i + 2
     int r = 2 * i + 2;
  
-    // If left child is larger than root
+    
     if (l < N && diff[l] > diff[largest])
         largest = l;
  
-    // If right child is larger than largest
-    // so far
+    
     if (r < N && diff[r] > diff[largest])
         largest = r;
  
-    // If largest is not root
+    
     if (largest != i) {
         swap(diff[i], diff[largest]);
         swap(r1[i], r1[largest]);
         swap(r2[i], r2[largest]);
-        // Recursively heapify the affected
-        // sub-tree
+        
         heapify(diff,r1,r2, N, largest);
     }
 }
 void heapSort(vector<int> &diff,vector<int> &r1,vector<int> &r2, int N)
 {
  
-    // Build heap (rearrange array)
+   
     for (int i = N / 2 - 1; i >= 0; i--)
         heapify(diff,r1,r2, N, i);
  
-    // One by one extract an element
-    // from heap
+   
     for (int i = N - 1; i > 0; i--) {
  
-        // Move current root to end
+        
         swap(diff[0], diff[i]);
         swap(r1[0], r1[i]);
         swap(r2[0], r2[i]);
-        // call max heapify on the reduced heap
+      
         heapify(diff,r1,r2, i, 0);
     }
 }
