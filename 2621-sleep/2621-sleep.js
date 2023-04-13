775c8ea79656d5984521a59b53936e584dcefd30
SHUBAM CHAUDHARY <110082517+shubamchaudhary@users.noreply.github.com>
2023-04-13 16:46:16 +0530

    Time: 62 ms (50.00%), Space: 41.5 MB (100.00%) - LeetHub

---
+++ 2621-sleep/2621-sleep.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number} millis
+ */
+async function sleep(millis) {
+   let p=new Promise(Resolve=>setTimeout(Resolve,millis));
+        return p;
+}
+
+/** 
+ * let t = Date.now()
+ * sleep(100).then(() => console.log(Date.now() - t)) // 100
+ */
